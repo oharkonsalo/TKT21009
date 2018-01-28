7958e8e76e5a8b2224566046ae56f35bd39e2476
oharkonsalo <oharkonsalo@yahoo.com>
2018-01-28 19:38:45 +0200

    tehtävä 1.12 anekdootit satunnaisesti

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,112 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    
-    constructor(props) {
-      super(props)
-      this.state = {
-        hyva: 0,
-        neutraali: 0,
-        huono: 0
-      }
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 5
     }
-    
-    klikHyva = () => {
+
+    } 
+    klikUusi = () => {
       this.setState({
-        hyva: this.state.hyva + 1
+        selected: Math.floor(Math.random() * (5 - 0 + 1)) + 0
+       
       })
     }
-    klikNeutraali = () => {
-        this.setState({
-        neutraali: this.state.neutraali + 1
-        })
-      }
-    klikHuono = () => {
-      this.setState({
-        huono: this.state.huono + 1
-      })
-    }
-    
-    render() {
-      const Button = ({ handleClick, text }) => (
-            <button onClick={handleClick}>
-              {text}
-            </button>
-       )
-       const Statistics = ({ luku, text }) => {
-        return (
-          <tr>
-            <td>{text}</td>
-            <td>{luku}</td>
-           
-          </tr>
-            
-        )
-       } 
-       const Statistic = ({ luku, paate, text }) => {
-        return (
-          <tr>
-            <td>{text}</td>
-            <td>{luku} {paate}</td>
-           
-          </tr>
-        )
-       } 
-       const KonditionaalinenRenderointi = () => {
-        if (this.state.hyva + this.state.neutraali + this.state.huono === 0) {
-          return (
-            <div>
-              <p>ei yhtään palautetta annettu</p>
-            </div>
-          )
-        }
-        return (
-          <div>
-            <table>
-             <tbody>
-              <Statistics luku={this.state.hyva} text="Hyvä"/>
-              <Statistics luku={this.state.neutraali} text="Neutraali"/>
-              <Statistics luku={this.state.huono} text="Huono"/>
-           
-           
-              <Statistic luku={((this.state.hyva * 1) + (this.state.neutraali * 0) + (this.state.huono * -1))/(this.state.hyva + this.state.neutraali + this.state.huono)} text="Keskiarvo"/>
-              <Statistic luku={((this.state.hyva / (this.state.neutraali + this.state.huono + this.state.hyva))*100)} text="Positiivisia" paate="%"/>
-            </tbody>
-           </table>
-          </div>
-          
-        )
-      }
 
-  
-      return (
-        <div>
-          <div>
-          <h1>Anna palautetta</h1>
-            <Button handleClick={this.klikHyva} text="Hyvä"/>
-            <Button handleClick={this.klikNeutraali} text="Neutraali"/>
-            <Button handleClick={this.klikHuono} text="Huono"/>
-           
-            <h1>Statistiikka</h1>
-           
-            
-        
-            <KonditionaalinenRenderointi />
- 
-          </div>
-        </div>
-      )
 
-    }
-  
-    
+
+  render() {
+    return (
+      <div>
+        {this.props.anecdotes[this.state.selected]}
+       <br></br> <button onClick={this.klikUusi}>next anecdote</button>
+
+      </div>
+    )
   }
+}
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-  
 
 ReactDOM.render(
-  <App />,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
