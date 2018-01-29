bdf8a3889b754ef0a02620dcfa810d74eecd3d8a
oharkonsalo <oharkonsalo@yahoo.com>
2018-01-29 18:18:04 +0200

    tehtava 1.14

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -5,13 +5,28 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 5
+      selected: 0,
+      pisteet: [0,0,0,0,0,0]
+
+      
     }
+    
 
     } 
-    klikUusi = () => {
+    klikNext = () => {
       this.setState({
         selected: Math.floor(Math.random() * (5 - 0 + 1)) + 0
+       
+      })
+    }
+    klikVote = () => {
+
+      let temp = this.state.pisteet
+      temp[this.state.selected] = temp[this.state.selected] + 1  
+
+      this.setState({
+
+        pisteet: temp
        
       })
     }
@@ -19,11 +34,19 @@ class App extends React.Component {
 
 
   render() {
+    
+    const MostVotes = () => {
+      let temp = this.state.pisteet
+      let suurin = temp.indexOf(Math.max(...this.state.pisteet))   
+      return <div>{this.props.anecdotes[suurin]}</div>
+    }
+
+
     return (
       <div>
-        {this.props.anecdotes[this.state.selected]}
-       <br></br> <button onClick={this.klikUusi}>next anecdote</button>
-
+        {this.props.anecdotes[this.state.selected]}<b> has {this.state.pisteet[this.state.selected]} votes</b>
+       <br></br><button onClick={this.klikVote}>vote anecdote</button> <button onClick={this.klikNext}>next anecdote</button>
+        <br></br> <h1>anecdote with most votes:</h1><br></br><MostVotes />
       </div>
     )
   }
