7eff48d53bd997b660e7d216f30c3823b209c6f7
oharkonsalo <oharkonsalo@yahoo.com>
2018-02-05 13:07:38 +0200

    2.15 fix

--- osa2/puhelinluettelo/puhelinluettelo/src/services/People.js
+++ osa2/puhelinluettelo/puhelinluettelo/src/services/People.js
@@ -8,9 +8,9 @@ const getAll = () => {
   const create = (newObject) => {
     return axios.post(baseUrl, newObject)
   }
-/*
+
   const update = (id, newObject) => {
     return axios.put(`${baseUrl}/${id}`, newObject)
   }
-*/
+
 export default { getAll, create, update }
